2d7e72c575b7d07571f1c6ff9afb31a754179d26
Adam Kazberuk <hodrobond@gmail.com>
2020-07-20 17:39:39 -0700

    Consistency

--- packages/DeveloperCard/src/index.tsx
+++ packages/DeveloperCard/src/index.tsx
@@ -9,6 +9,7 @@ const CardContainer = styled.div`
   box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
   display: flex;
   align-items: center;
+  margin: 1rem;
 `;
 
 const ImageWRapper = styled.img`
