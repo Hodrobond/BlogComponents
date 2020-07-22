27f5668a2e5b7653b1f5e2c6fe8b872454559d7d
Adam Kazberuk <hodrobond@gmail.com>
2020-07-21 19:02:28 -0700

    Temp

--- packages/RotationWrapper/__stories__/index.stories.tsx
+++ packages/RotationWrapper/__stories__/index.stories.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState } from "react";
 import { withKnobs, color } from "@storybook/addon-knobs";
 import RotationWrapper from '../src';
 import HamburgerIcon from '@hodrobond/ui-hamburgericon';
+import DeveloperCard from '@hodrobond/ui-developercard';
 
 export default {
   component: RotationWrapper,
@@ -21,6 +22,21 @@ const WithClick = () => {
   )
 }
 
+const WithClick2 = () => {
+  const [stateRotate, setRotate] = useState(false);
+  return (
+    <RotationWrapper
+      onClick={() => setRotate(!stateRotate)}
+      rotate={stateRotate}
+    >
+      <DeveloperCard/>
+    </RotationWrapper>
+  )
+}
 export const RotationWrapperStory = () => (
   <WithClick/>
 );
+
+export const AdamTest23 = () => (
+  <WithClick2/>
+)
