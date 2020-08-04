afb348cbde7b58fae83336d2ce6baac24ce6a032
Adam Kazberuk <hodrobond@gmail.com>
2020-08-03 19:53:18 -0700

    Typings

--- packages/DateDisplay/src/index.tsx
+++ packages/DateDisplay/src/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment/min/moment-with-locales';
+import moment from 'moment';
+import 'moment/min/locales';
 import Text from '@hodrobond/ui-text';
 
 export interface DateDisplayInterface {
